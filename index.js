9a90c571217cf79e08656b8b225289ae0e355dc7
b_g <b_g@dsm.hs.kr>
2024-10-15 23:27:58 +0900

    feat: add search and img func

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,6 +12,39 @@ const io = socketIo(server);
 const PORT = 3000;
 
 app.use(express.static('public'));
+
+async function fetchFirstImage(query) {
+  const searchUrl = `https://www.google.com/search?hl=en&tbm=isch&q=${encodeURIComponent(query)}`;
+
+  try {
+    const { data } = await axios.get(searchUrl);
+    const $ = cheerio.load(data);
+    const image = $('img').eq(1).attr('src');
+    return image || '이미지를 찾을 수 없습니다.';
+  } catch (error) {
+    return '이미지를 가져오는 중 오류가 발생했습니다.';
+  }
+}
+
+const isAllowedRequest = (req, res, next) => {
+  const referer = req.get('Referer');
+  const allowedOrigin = 'http://localhost:3000/' // 'https://status-chat.ijw.app/';
+  if (referer && referer.startsWith(allowedOrigin)) {
+    return next();
+  }
+  return res.status(403).json({ error: '요청이 거부되었습니다.' });
+};
+
+app.get('/api/search', isAllowedRequest, async (req, res) => {
+  const query = req.query.q;
+
+  if (!query) {
+    return res.status(400).json({ error: '검색어를 제공해 주세요.' });
+  }
+
+  const imageUrl = await fetchFirstImage(query);
+  res.json({ imageUrl });
+});
 
 app.get('/api/status-codes', (req, res) => {
   const filePath = path.join(__dirname, 'public', 'status-code.json');
@@ -27,5 +62,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`listening...`);
+  console.log(`listening on port ${PORT}...`);
 });
